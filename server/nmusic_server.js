79457d4ed7d3cbdeeca8cbabb5c85b033db91801
Fela Maslen <felamaslen@gmail.com>
2015-08-12 21:51:03 +0200

    Added Access-Control-Allow-Origin headers to server

--- server/nmusic_server.js
+++ server/nmusic_server.js
@@ -209,6 +209,19 @@ const handleRequest = (req, res) => {
 
   const path = parts[0].substring(1);
 
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
   switch (method) {
   case 'POST':
     res.status(500).end('POST not implemented');
