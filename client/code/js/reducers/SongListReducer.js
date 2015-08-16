853cbcedc3902612b092224c4758c3191f4bc8b3
Fela Maslen <felamaslen@gmail.com>
2015-08-16 21:43:34 +0200

    Shift selecting

--- client/code/js/reducers/SongListReducer.js
+++ client/code/js/reducers/SongListReducer.js
@@ -12,30 +12,53 @@ export default buildReducer({
   [LIST_SONG_SELECTED]: (reduction, evt) => {
     let newRanges;
 
+    const ranges = reduction.getIn(['appState', 'songList', 'selectedSongs']);
+    const inRange = itemInRanges(ranges, evt.index);
+
     if (evt.ctrl) {
-      const ranges = reduction.getIn(['appState', 'songList', 'selectedSongs']);
-
-      const inRange = itemInRanges(ranges, evt.index);
-
       if (inRange > -1) {
         // deselect item
         const splitRange = ranges.get(inRange);
         const splitLeft = splitRange.first();
         const splitRight = splitRange.last();
 
-        newRanges = ranges.splice(
-          inRange,
-          1,
-          fromJS([splitLeft, evt.index - 1]),
-          fromJS([evt.index + 1, splitRight])
-        );
+        const args = [inRange, 1];
+
+        if (splitLeft < evt.index) {
+          args.push(fromJS([splitLeft, evt.index - 1]));
+        }
+
+        if (splitRight > evt.index) {
+          args.push(fromJS([evt.index + 1, splitRight]));
+        }
+
+        newRanges = ranges.splice.apply(ranges, args);
       } else {
         // select item
         newRanges = ranges.push(fromJS([evt.index, evt.index]));
       }
     } else if (evt.shift) {
-      console.debug('shift not implemented');
-      newRanges = ranges;
+      if (inRange > -1) {
+        newRanges = ranges;
+      } else {
+        // find end of last range
+        let lastRange = null;
+
+        ranges.forEach((range, index) => {
+          const rangeStart = range.first();
+          if (rangeStart < evt.index && (lastRange === null || rangeStart > lastRange[1])) {
+            lastRange = [index, rangeStart];
+          }
+        });
+
+        if (lastRange !== null) {
+          const newLastRange = fromJS([lastRange[1], evt.index]);
+
+          newRanges = ranges.splice(lastRange[0], 1, newLastRange);
+        } else {
+          newRanges = ranges;
+        }
+      }
     } else {
       newRanges = fromJS([[evt.index, evt.index]]);
     }
