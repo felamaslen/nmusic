3c1394a7721da53770bd340277f513a60e8611d7
Fela Maslen <felamaslen@gmail.com>
2015-08-22 13:27:29 +0200

    Next, previous basic implementation

--- client/code/js/reducers/PlayerReducer.js
+++ client/code/js/reducers/PlayerReducer.js
@@ -40,13 +40,73 @@ export const togglePause = (reduction, paused) => {
 };
 
 export const ctrlPrevious = reduction => {
-  console.debug('PLAYER_CTRL_PREVIOUS_CLICKED');
-  return reduction;
+  const currentSongId = reduction.getIn(['appState', 'player', 'currentSongId']);
+  const queue = reduction.getIn(['appState', 'player', 'queue']);
+
+  let newCurrentSongId = currentSongId - 1;
+  let newQueue;
+
+  if (newCurrentSongId > -1) {
+    // play previous song in queue
+    newQueue = queue;
+  } else {
+    // play previous song in songlist, if it exists, since
+    // we are already at the bottom of the queue
+    const currentId = queue.getIn([currentSongId, 'id']);
+    const songList = reduction.getIn(['appState', 'songList', 'list']);
+    const songId = songList.findIndex(
+      song => song.get('id') === currentId
+    );
+
+    if (songId > 0) {
+      newCurrentSongId = 0;
+      newQueue = queue.unshift(songList.get(songId - 1));
+    } else {
+      // nothing to play; clear the queue
+      newCurrentSongId = -1;
+      newQueue = List.of();
+    }
+  }
+
+  return reduction
+    .setIn(['appState', 'player', 'currentSongId'], newCurrentSongId)
+    .setIn(['appState', 'player', 'queue'], newQueue)
+  ;
 };
 
 export const ctrlNext = reduction => {
-  console.debug('PLAYER_CTRL_NEXT_CLICKED');
-  return reduction;
+  const currentSongId = reduction.getIn(['appState', 'player', 'currentSongId']);
+  const queue = reduction.getIn(['appState', 'player', 'queue']);
+
+  let newCurrentSongId = currentSongId + 1;
+  let newQueue;
+
+  if (currentSongId < queue.size - 1) {
+    // play next song in queue
+    newQueue = queue;
+  } else {
+    // play next song in songlist, if it exists, since
+    // we are already at the end of the queue
+    const currentId = queue.getIn([currentSongId, 'id']);
+    const songList = reduction.getIn(['appState', 'songList', 'list']);
+    const songId = songList.findIndex(
+      song => song.get('id') === currentId
+    );
+
+    if (songId > -1 && songId < songList.size - 1) {
+      newCurrentSongId = queue.size;
+      newQueue = queue.push(songList.get(songId + 1));
+    } else {
+      // nothing to play; clear the queue
+      newCurrentSongId = -1;
+      newQueue = List.of();
+    }
+  }
+
+  return reduction
+    .setIn(['appState', 'player', 'currentSongId'], newCurrentSongId)
+    .setIn(['appState', 'player', 'queue'], newQueue)
+  ;
 };
 
 export const ctrlSeek = (reduction, position) => {
