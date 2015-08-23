74171acdbdbbd583862e164194e70bda41a3c6cb
Fela Maslen <felamaslen@gmail.com>
2015-08-23 11:40:38 +0200

    Added messages to config.js on client

--- client/code/js/config.js
+++ client/code/js/config.js
@@ -2,15 +2,15 @@
  * config parameters
  */
 
+/* You can modify these settings */
 export const PREVIOUS_SONG_DELAY = 2.5;
-
 export const NAVIGATION_WARNING_MESSAGE = 'This action will stop the music.';
 
+/* Don't modify anything beyond here */
 const REQ = 'http://localhost:3005';
+const API = REQ + '/api';
 
 export const AUTH_AUTHENTICATE = REQ + '/authenticate';
-
-const API = REQ + '/api';
 export const API_LIST_ARTISTS = API + '/list/artists';
 export const API_LIST_SONGS_FROM_BROWSER = API + '/list/songs/';
 export const STREAM_URL = API + '/play/';
